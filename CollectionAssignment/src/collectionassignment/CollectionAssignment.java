07dbf9a316994973f23e44ea084fbbfd32a66480
jeriabegglen <jeriabegglen@gmail.com>
2016-11-08 17:29:47 -0700

    set up to begin j unit testing for collection

---
+++ CollectionAssignment/src/collectionassignment/CollectionAssignment.java
@@ -0,0 +1,15 @@
+
+package collectionassignment;
+
+/**
+ *
+ * @author jeriabegglen
+ */
+public class CollectionAssignment {
+
+    public int convert(String s1){
+        int int1 = Integer.parseInt(s1);
+        return 0;
+    }
+    
+}
