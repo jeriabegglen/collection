be2b758681cbea392150042463c0ae7f01350fe8
jeriabegglen <jeriabegglen@gmail.com>
2016-11-08 17:32:47 -0700

    first j unit test, it failed!  That is a good thing.  Now I can start writing tests that will pass.

---
+++ CollectionAssignment/test/collectionassignment/CollectionAssignmentTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collectionassignment;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jeriabegglen
+ */
+public class CollectionAssignmentTest {
+    
+    public CollectionAssignmentTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of convert method, of class CollectionAssignment.
+     */
+    @Test
+    public void testConvert() {
+        System.out.println("convert");
+        String s1 = "9360";
+        CollectionAssignment instance = new CollectionAssignment();
+        int expResult = 0;
+        int result = instance.convert(s1);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
