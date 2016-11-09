18b2f9e5db1c09d572b4452ff0095ed34b6929d1
jeriabegglen <jeriabegglen@gmail.com>
2016-11-08 17:58:21 -0700

    I had to do some adjusting of data types, but I think I figured it out.

--- CollectionAssignment/src/collectionassignment/CollectionAssignment.java
+++ CollectionAssignment/src/collectionassignment/CollectionAssignment.java
@@ -7,9 +7,16 @@ package collectionassignment;
  */
 public class CollectionAssignment {
 
-    public int convert(String s1){
+    public String convert(String s1){
         int int1 = Integer.parseInt(s1);
-        return 0;
+        int hours = 0, remainder = 0, minutes = 0, seconds = 0;
+        String dummy;
+        hours = int1 / 3600;
+        remainder = int1 % 3600;
+        minutes = remainder / 60;
+        seconds = remainder % 60;
+        dummy = "" + hours + minutes + seconds;
+        return dummy;
     }
     
 }
